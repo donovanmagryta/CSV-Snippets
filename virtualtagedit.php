d06cf6b8a78adfb85d561662fc7c99020f0d17e7
Donovan <triops124@gmail.com>
2019-04-21 16:17:42 -0400

    Update virtualtagedit.php

--- virtualtagedit.php
+++ virtualtagedit.php
@@ -9,13 +9,13 @@ $data[$tagid] = $tagvalue;
 $newjson = json_encode($data); 
 file_put_contents('tags.json', $newjson); 
 fclose($file); 
-echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post">edit scanned tag\'s value<input type="text" name="tagvalue" value="'.$tagvalue.'"><br><input type="submit"></form><button><a href = "main.php">go to main page</a></button></body></html>'; 
+echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post">edit scanned tag\'s value<input type="text" name="tagvalue" value="'.$tagvalue.'"><br><input type="submit"></form><button><a href = "index.php">go to main page</a></button></body></html>'; 
 }
  else if(!isset($_POST["tagvalue"])) { 
 $file = fopen("tags.json","a+") or die ("file not found"); 
 $json = file_get_contents('tags.json'); $data = json_decode($json, true); 
 @$output = $data["$tagid"];
- echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post">edit scanned tag\'s value<input type="text" name="tagvalue" value="'.$output.'"><br><input type="submit"></form><button><a href = "main.php">go to main page</a></button> </body></html>'; 
+ echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post">edit scanned tag\'s value<input type="text" name="tagvalue" value="'.$output.'"><br><input type="submit"></form><button><a href = "index.php">go to main page</a></button> </body></html>'; 
 fclose($file); 
 } 
 ?>
