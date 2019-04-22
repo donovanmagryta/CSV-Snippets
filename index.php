d1eb218c28995999f6c88aec079f86f8e7bbe5f7
Donovan <triops124@gmail.com>
2019-04-22 19:37:03 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -14,18 +14,19 @@ $piece = explode('+',$tagval);
 $studentfile = $piece[0];
 $skill = $piece[1];
 $grade = $piece[2];
-if (!file_exists("'.$studentfile.'.csv")) {
-$file = fopen("'.$studentfile.'.csv","a");
+$sheet = $studentfile.'.csv';
+if (!file_exists($sheet)) {
+$file = fopen($sheet,"a");
  $stamp = date("m-d-y h:i:s");
 $feed = ("TIMESTAMP" .",". "," .  "SKILL" . ",". "GRADE" . PHP_EOL);
-file_put_contents("'.$studentfile.'.csv", $feed, FILE_APPEND);
+file_put_contents($sheet, $feed, FILE_APPEND);
 fclose($file);
 }
  if ($adminlogin == 'password123') {
-   echo "<html><style>body { Font-size: 300%; Font-family: Arial; text-decoration: none; } button { text-decoration: none; text-align: center; font-size: 100%; margin: 50px; background-color: navy; color: white; border-radius: 12px; } a { text-decoration: none; color: white;}</style><body> <h1><button><a href = \"'.$studentfile.'.csv\"> Download responses</a></button><button><a href=\"?delete=1&adminlogin=password123&tag=no\">delete data</a> </button></h1><button><a href=\"tagedit.php\">Switch Student Reprogram Tag</a></button></body></html>";
- }
+   echo "<html><style>body { Font-size: 300%; Font-family: Arial; text-decoration: none; } button { text-decoration: none; text-align: center; font-size: 100%; margin: 50px; background-color: navy; color: white; border-radius: 12px; } a { text-decoration: none; color: white;}</style><body> <h1><button><a href = \"\'.$sheet.'\"> Download responses</a></button><button><a href=\"?delete=1&adminlogin=password123&tag=no\">delete data</a> </button></h1><button><a href=\"tagedit.php\">Switch Student Reprogram Tag</a></button></body></html>";
+ } 
  if (isset($_GET['delete'])) {
-   $myFile= "'.$studentfile.'.csv";
+   $myFile= $sheet;
 unlink($myFile) or die ("error: cannot delete spreadsheet");
 echo "spreadsheet reset";
 }
@@ -33,10 +34,10 @@ echo "spreadsheet reset";
 exit("no tag left");
  }
 else {
- $file = fopen("'.$studentfile.'.csv","a");
+ $file = fopen($sheet,"a");
  $stamp = date("m-d-y h:i:s");
-$feed = ($stamp .",". "," .  $skill . ",". $grade . PHP_EOL);
-file_put_contents("'.$studentfile.'.csv", $feed, FILE_APPEND);
+$feed = ($stamp .",". "," . $skill . ",". $grade . PHP_EOL);
+file_put_contents($sheet, $feed, FILE_APPEND);
 fclose($file);
 echo "<html><style>body { font-size: 300%; font-family: Arial;}button { text-decoration: none; text-align: center; font-size: 100%; margin: 50px; background-color: navy; color: white; border-radius: 12px; } a { text-decoration: none; color: white;} </style><body> <h1> Tag recorded</h1></body></html>";
 echo $tag;
